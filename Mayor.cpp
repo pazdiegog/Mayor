e270aa4a9d0e89e2fee6676a9f342caa35c53c0f
Diego Paz <pazdiegog@est.frba.utn.edu.ar>
2017-04-13 00:35:38 -0300

    Se agregaron comentarios en el codigo

--- Mayor.cpp
+++ Mayor.cpp
@@ -1,5 +1,7 @@
 /*
-Created by Diego on 12/4/2017.
+Mayor.cpp
+Diego G Paz
+12/4/2017.
 Se ingresan 2 numeros y se imprime en pantalla cual de los dos es mayor
 */
 
@@ -9,12 +11,12 @@ int main()
 {
     {
     float a, b, mayor;                         /*declaracion de las variables a utilizar en el proceso del programa*/
-    std::cout << "Este programa elige entre dos numeros, cual es el mayor \n";       /*Se pide el ingreso de los numeros enteros*/
+    std::cout << "Este programa elige entre dos numeros, cual es el mayor \n";       /*Se pide el ingreso de los numeros reales*/
     std::cout << "Ingrese un numero: ";
     std::cin >> a;
     std::cout << "Ingrese otro numero: ";
     std::cin >> b;
-    if (a>b)
+    if (a>b)                                   /*Se comparan los valores ingresados y se imprime el numero mayor*/
         {mayor = a;
         std::cout<< "El numero "<<mayor<<" es el mayor entre "<<a<<" y "<<b<<".";
         }
