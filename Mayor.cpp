65696dd15180f6e3c8c55cca6d0cbc705f732e06
Diego Paz <pazdiegog@est.frba.utn.edu.ar>
2017-04-13 00:19:51 -0300

    Se agrego una variable para hacer mas amigable el codigo. Se cambio el formato de lo que se imprimia en pantalla. Se modifica el readme.md con enunciado, algoritmo y analisis.

--- Mayor.cpp
+++ Mayor.cpp
@@ -8,19 +8,20 @@ Se ingresan 2 numeros y se imprime en pantalla cual de los dos es mayor
 int main()
 {
     {
-    float a, b;                         /*declaracion de las variables a utilizar en el proceso del programa*/
+    float a, b, mayor;                         /*declaracion de las variables a utilizar en el proceso del programa*/
     std::cout << "Este programa elige entre dos numeros, cual es el mayor \n";       /*Se pide el ingreso de los numeros enteros*/
     std::cout << "Ingrese un numero: ";
     std::cin >> a;
     std::cout << "Ingrese otro numero: ";
     std::cin >> b;
     if (a>b)
-        {
-        std::cout<< "El numero "<<a<<" es mayor a "<<b<<".";
+        {mayor = a;
+        std::cout<< "El numero "<<mayor<<" es el mayor entre "<<a<<" y "<<b<<".";
         }
     else if (a<b)
         {
-        std::cout<< "El numero "<<b<<" es mayor a "<<a<<".";
+        mayor = b;
+        std::cout<< "El numero "<<mayor<<" es el mayor entre "<<a<<" y "<<b<<".";
         }
     else{
         std::cout<< "Los numeros "<<a<<" y "<<b<<" son iguales.";
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Mayor
-Dado dos números informar cuál es el mayor
+
+*Enunciado: Dado dos números informar cuál es el mayor
+
+*Analisis:
+
+---a---b--->(P)---a,b--->
+
+a,b ϵ R.
+
+*Algoritmo:
+
+-Read a
+
+-Read b
+
+-mayor= a>b o mayor= a<b
+
+-Write mayor.
